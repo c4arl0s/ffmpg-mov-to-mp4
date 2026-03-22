9ced76a58fae8b0075732126e184163167717e5c
c4arl0s <c.santiago.cruz@gmail.com>
2026-03-22 12:55:15 -0600

    Verify when first parameter is empty

--- ffmpg-mov-to-mp4.sh
+++ ffmpg-mov-to-mp4.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]; then
+    echo "Warning: no file path was provided (first argument is empty)." >&2
+    exit 1
+fi
+
 FILE_PATH=$1
 FILE_NAME_WITH_EXTENSION=$(basename "$FILE_PATH")
-FILE_NAME=$(echo $FILE_NAME_WITH_EXTENSION | cut -d "." -f 1)    
+FILE_NAME=$(echo "$FILE_NAME_WITH_EXTENSION" | cut -d "." -f 1)
 extension=$(echo "$1" | cut -d "." -f 2)
-if [ $extension = "mov" ]; then 
+
+if [ "$extension" = "mov" ]; then
     ffmpeg -i "$1" -f mp4 "$FILE_NAME.mp4"
-else 
-    echo "$FILE_NAME has not an mp4 extension"
+else
+    echo "$FILE_NAME does not have a mov extension"
 fi
